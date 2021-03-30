e603778b388c34e69542fe4390edd1beb4b1cea4
mits <dmitrybaycharov@gmail.com>
2021-03-30 12:46:12 -0500

    Random Word finished

--- wordfinder.py
+++ wordfinder.py
@@ -1,5 +1,20 @@
 """Word Finder: finds random words from a dictionary."""
-
+from random import choice
 
 class WordFinder:
-    ...
+    """"""
+    def __init__(self, url):
+        """initializes and opens a file"""
+        self.url = url
+        with open(url) as f:
+            self.lines = f.readlines()
+        self.words_read = len(self.lines)
+
+    def __repr__(self):
+        return f'{self.words_read} words read'
+
+    def random(self):
+        """"""
+        return choice(self.lines).strip()
+
+    
